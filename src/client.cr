459910b85d6da8fdd86c962240e72136b8255612
Jared Smith <dev@jaredsmith.se>
2019-10-13 14:56:31 -0700

    Add Habitat configs

---
+++ src/client.cr
@@ -0,0 +1,7 @@
+module Monzo
+  Habitat.create do
+    setting user_id : String
+    setting account_id : String
+    setting access_token : String
+  end
+end
--- src/monzo.cr
+++ src/monzo.cr
@@ -1,6 +1,6 @@
+require "habitat"
+
 # TODO: Write documentation for `Monzo.cr`
 module Monzo
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
 end
