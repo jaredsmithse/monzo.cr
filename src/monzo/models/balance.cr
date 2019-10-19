480c05d7a51bdf7df8b1ac1b767956172fac8208
Jared Smith <dev@jaredsmith.se>
2019-10-19 16:11:22 -0700

    :memo: Added docs for Balance from https://docs.monzo.com/#read-balance

--- src/monzo/models/balance.cr
+++ src/monzo/models/balance.cr
@@ -2,17 +2,24 @@ class Balance
   include JSON::Serializable
 
   @[JSON::Field(key: "balance")]
+  # The currently available balance of the account, as an 64bit integer in minor
+  # units of the currency, eg. pennies for GBP, or cents for EUR and USD.
   property balance_cents : Int64
 
   @[JSON::Field(key: "total_balance")]
+  # The sum of the currently available balance of the account and the combined
+  # total of all [the user’s pots](https://monzo.com/docs/#list-pots).
   property total_balance_cents : Int64
 
   @[JSON::Field(key: "balance_including_flexible_savings")]
   property balance_including_flexible_savings_cents : Int64
 
+  # The ISO 4217 currency code.
   property currency : String
 
   @[JSON::Field(key: "spend_today")]
+  # The amount spent from this account today (considered from approx 4am
+  # onwards), as a 64bit integer in minor units of the currency.
   property spend_today_cents : Int64
 
   property local_currency : String
